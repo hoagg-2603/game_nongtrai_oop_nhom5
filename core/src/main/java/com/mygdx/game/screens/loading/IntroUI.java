37e078c3f1d33b1f5aa54a8321fbc94ed24e2e62
duc khui <itsduckhoi04@gmail.com>
2024-11-24 20:17:14 +0700

    update tiep

--- core/src/main/java/com/mygdx/game/screens/loading/IntroUI.java
+++ core/src/main/java/com/mygdx/game/screens/loading/IntroUI.java
@@ -9,7 +9,7 @@ public class IntroUI extends Window {
 
     private Table table;
     private TextButton quitButton, confirmButton; //Xác định nút văn bản，Dùng để đóng cửa sổ sử dụng
-    private Label label;           //Xác định nhãn，để thêm văn bản
+    private Label label; //Xác định nhãn，để thêm văn bản
 
 
     public IntroUI(String title, Skin skin){
@@ -24,10 +24,10 @@ public class IntroUI extends Window {
         quitButton.setScale(0.8f);
         quitButton.setPosition(80,5);
 
-        table.setSize(500,500);
+        table.setSize(500,500); 
         table.setPosition( Gdx.graphics.getWidth()/2-225, Gdx.graphics.getHeight()/2-225);
 
-        label=new Label("   Cài đặt có thể được sử dụng để tùy chỉnh,\n   hình đại diện, âm nhạc.\n   Sử dụng các phím lên, xuống, trái, phải\n    để di chuyển, Có thể ngủ hoặc bổ sung \n    thức ăn để khôi phục thể lực."  +  "\n    Nhân vật di chuyển trên bản đồ,\n    có thể nói chuyện với NPC.\n    Cây trồng cần được tưới nươc.\n      Nếu không, sẽ bị héo úa.\n", skin);
+        label=new Label("Cài đặt có thể được sử dụng để tùy chỉnh\nhình đại diện, âm nhạc. Sử dụng các phím\nlên, xuống, trái, phải để di chuyển.\n\nCó thể ngủ hoặc bổ sung thức ăn để khôi\nphục thể lực. Nhân vật di chuyển trên bản\nđồ có thể nói chuyện với NPC. Cây trồng\ncần được tưới nước, nếu không sẽ bị héo.", skin);
 
         quitButton = new TextButton("Đóng", skin);
         quitButton.setTransform(true);
